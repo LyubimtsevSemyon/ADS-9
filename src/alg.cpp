54d4912c0e0fc1b0b9c9e2c98171db14f7079a14
LyubimtsevSemyon <99214966+LyubimtsevSemyon@users.noreply.github.com>
2022-05-28 19:06:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,23 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+  BST<std::string> bst;
+  std::ifstream file(filename);
+  std::string word = "";
+  if (!file.is_open()) {
+    throw std::string("Error! File did not open!");
+  }
+  while (!file.eof()) {
+    word = "";
+    while (true) {
+      char pod = file.get();
+      if ((pod >= 65 && pod <= 90) || (pod >= 97 && pod <= 122)) {
+        word += tolower(pod);
+      } else {
+        break;
+      }
+    }
+    bst.Add(word);
+  }
+  return bst;
 }
