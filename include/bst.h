d2be4b410e7a463b30a24cfa063d2b43ed3c9ab9
LyubimtsevSemyon <99214966+LyubimtsevSemyon@users.noreply.github.com>
2022-05-28 19:24:48 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -7,10 +7,10 @@ template<typename T>
 class BST {
  private:
   struct Node {
-    T value;
-    int count = 0;
-    Node* left = nullptr;
-    Node* right = nullptr;;
+  T value;
+  int count = 0;
+  Node* left = nullptr;
+  Node* right = nullptr;;
   };
   Node* root;
   Node* addNode(Node* root, const T& val) {
@@ -20,39 +20,39 @@ class BST {
       root->count = 1;
       root->left = root->right = nullptr;
     } else if (root->value < val) {
-      root->left = addNode(root->left, val);
-    } else if (root->value > val) {
-      root->right = addNode(root->right, val);
-    } else {
-      root->count++;
+        root->left = addNode(root->left, val);
+      } else if (root->value > val) {
+          root->right = addNode(root->right, val);
+        } else {
+            root->count++;
+          }
+    return root;
     }
-      return root;
-  }
-  int searchNode(Node* root, const T& val) {
-    if (root == nullptr) {
-      return 0;
-    } else if (root->value == val) {
-      return root->count;
-    } else if (root->value < val) {
-      return searchNode(root->left, val);
-    } else {
-      return searchNode(root->right, val);
+    int searchNode(Node* root, const T& val) {
+      if (root == nullptr) {
+        return 0;
+      } else if (root->value == val) {
+          return root->count;
+        } else if (root->value < val) {
+            return searchNode(root->left, val);
+          } else {
+              return searchNode(root->right, val);
+            }
     }
-  }
-  int depth_p(Node* root) {
-    int a = 0, b = 0;
-    if (root == nullptr) {
-      return 0;
-    } else {
-      a = depth_p(root->left);
-      b = depth_p(root->right);
+    int depth_p(Node* root) {
+      int lft = 0, rht = 0;
+      if (root == nullptr) {
+        return 0;
+      } else {
+          lft = depth_p(root->left);
+          rht = depth_p(root->right);
+        }
+        if (rht > lft) {
+            return ++rht;
+        } else {
+            return ++lft;
+          }
     }
-    if (b > l) {
-      return ++b;
-    } else {
-      return ++a;
-    }
-  }
 
  public:
   BST() :root(nullptr) {}
@@ -63,7 +63,7 @@ class BST {
     return searchNode(root, val);
   }
   int depth() {
-    return depth_p(root)-1;
+    return depth_p(root) - 1;
   }
 };
 
